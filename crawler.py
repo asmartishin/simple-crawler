42316e2b1e5278bf45ae72ed51bccc3ef46aba3f
Alexey Martishin <monkey>
2016-10-12 11:55:59 +0300

    implemented reverse index creation

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ import requests
 import json
 import argparse
 from threading import Thread
-from lib.utils import str2date, date2timestamp, get_username
+from lib.utils import string_to_date, date_to_timestamp, get_username, string_to_hash, remove_whitespaces
 from lib.logger import Logger
 from lxml import html, etree
 from pprint import pprint
@@ -14,48 +14,68 @@ from datetime import datetime, timedelta
 class Crawler(object):
     def __init__(self, args, config):
         self.config = config
+        self.logger = Logger('logs/crawler.log').log
         self.main_page_selectors = config['selectors']['main_page']
+        self.post_page_selectors = config['selectors']['post_page']
         self.base_url = config['base_url']
+        self.data_from_main_page = []
+        self.inverted_index = {}
         if args.daemon:
             self.data_update_interval = config['data_update_interval']
-            self._spawn_cache_updater()
+            self._spawn_data_updater()
 
-    def crawl(self):
+    def get_data(self):
         url = self.base_url + self.config['index_url']
-        result = []
+        min_timestamp = date_to_timestamp(datetime.now() - timedelta(1))
+        data_from_posts = []
+        self.data_from_main_page = self.get_data_from_main_page(url, min_timestamp)
+        for post_data in self.data_from_main_page:
+            document_id = post_data['document_id']
+            content = self.get_data_from_post(post_data['document_url'])
+            self.create_inverted_index(content, document_id, self.inverted_index)
+        pprint(self.inverted_index)
 
-        min_timestamp = date2timestamp(datetime.now() - timedelta(1))
+    def get_data_from_main_page(self, url, min_timestamp):
+        data_from_main_page = []
+        data_from_posts = []
         continue_parsing = True
-
         while continue_parsing:
-            result, url, continue_parsing = self.get_data_from_main_page(result, url, min_timestamp)
+            tree = etree.HTML(requests.get(url).text)
+            for post in Crawler.dom_element_get_children(tree, self.main_page_selectors['post']):
+                create_time = string_to_date(Crawler.dom_element_get_children(post, self.main_page_selectors['create_time'])[0].text)
+                ctime = date_to_timestamp(create_time)
 
-        pprint(result)
+                if ctime < min_timestamp:
+                    continue_parsing = False
+                    break
 
-    # didn't use recursion, because of call stack overflow
-    def get_data_from_main_page(self, result, url, min_timestamp):
+                post_title = Crawler.dom_element_get_children(post, self.main_page_selectors['post_title'])[0]
+                post_author = Crawler.dom_element_get_children(post, self.main_page_selectors['post_title'])[0]
+                post_url = post_title.get('href')
+                document_id = string_to_hash(post_title.get('href'))
+
+                data_from_main_page.append({
+                    'create_time': str(create_time),
+                    'ctime': ctime,
+                    'post_title': post_title.text,
+                    'document_url': post_url,
+                    'document_id': document_id,
+                    'post_author': get_username(post_author.get('href')).lower()
+                })
+            next_page = Crawler.dom_element_get_children(tree, self.main_page_selectors['next_page'])[0]
+            url = self.base_url + next_page.get('href')
+        return data_from_main_page
+
+    def get_data_from_post(self, url):
         tree = etree.HTML(requests.get(url).text)
-        for post in Crawler.dom_element_get_children(tree, self.main_page_selectors['post']):
-            create_time = str2date(
-                Crawler.dom_element_get_children(post, self.main_page_selectors['create_time'])[0].text)
-            ctime = date2timestamp(create_time)
+        post_content = remove_whitespaces("".join(tree.xpath('//{}[contains(@class, "{}")]/text()'.format(
+                self.post_page_selectors['content']['tag'], self.post_page_selectors['content']['selector'])))).strip()
+        return post_content
 
-            if ctime < min_timestamp:
-                return (result, url, False)
-
-            post_title = Crawler.dom_element_get_children(post, self.main_page_selectors['post_title'])[0]
-            post_author = Crawler.dom_element_get_children(post, self.main_page_selectors['post_title'])[0]
-
-            result.append({
-                'create_time': str(create_time),
-                'ctime': ctime,
-                'title': post_title.text,
-                'href': post_title.get('href'),
-                'author': get_username(post_author.get('href')).lower()
-            })
-        next_page = Crawler.dom_element_get_children(tree, self.main_page_selectors['next_page'])[0]
-        url = self.base_url + next_page.get('href')
-        return (result, url, True)
+    def create_inverted_index(self, content, document_id, inverted_index):
+        forward_index = {index: word for index, word in enumerate(content.split())}
+        for key, value in forward_index.items():
+            inverted_index.setdefault(value, {}).setdefault(document_id, []).append(key)
 
     @staticmethod
     def dom_element_get_children(root, selector_data):
@@ -84,6 +104,5 @@ if __name__ == '__main__':
     args = parse_arguments()
     with open('conf/crawler.conf.json') as conf_file:
         config = json.load(conf_file)
-    logger = Logger('logs/crawler.log')
     crawler = Crawler(args, config)
-    crawler.crawl()
+    crawler.get_data()
--- lib/utils.py
+++ lib/utils.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from dateutil import parser
 from urllib.parse import urlparse
 import re
+import hashlib
+
 
 format_time = {
     'января': 'January',
@@ -23,16 +25,23 @@ format_time = {
 }
 
 
-def str2date(stime):
+def string_to_date(stime):
     format_time['сегодня'] = datetime.now().strftime('%Y-%m-%d')
     format_time['вчера'] = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
     pattern = "|".join(re.escape(x) for x in format_time)
     return parser.parse(re.sub(pattern, lambda x: format_time.get(x.group(0).lower()), stime))
 
 
-def date2timestamp(dtime):
+def date_to_timestamp(dtime):
     return int(time.mktime(dtime.timetuple()))
 
 
 def get_username(url):
     return urlparse(url.rstrip('/')).path.rpartition('/')[2]
+
+
+def string_to_hash(string):
+    return hashlib.md5(string.encode('utf-8')).hexdigest()
+
+def remove_whitespaces(string):
+    return re.sub(r'\s+', ' ', string)
